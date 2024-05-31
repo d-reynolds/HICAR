cc7aab4ad61d5fe74d35be335d46add1be68cbad
Dylan Reynolds <dylan.reynolds@slf.ch>
2024-05-31 17:52:36 +0200

    Added interactive script  (helpers/gen_HICAR_dir.sh) for generating a file system to conduct HICAR runs from

--- helpers/filelist_script.sh
+++ helpers/filelist_script.sh
@@ -7,11 +7,11 @@
 pattern=$1
 out_file=$2
 temp_file="temp1223334332.txt"
-echo $pattern
+#echo $pattern
 readlink -f $pattern > $temp_file
 
 while read line; do
-	echo "\"$line\""
+#	echo "\"$line\""
         echo "\"$line\"" >> $out_file
 done <$temp_file
 rm $temp_file
